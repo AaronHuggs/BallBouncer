c0d997eb4b78ab1017a93c0b613ad91670d8f6c2
Aaron Huggins <huggins.aaron@outlook.com>
2021-08-16 20:33:24 -0700

    Fixed the build so that it compiles and everything correctly

--- BallBouncer/build.bat
+++
@@ -1,3 +0,0 @@
-ca65 BallBouncer.asm -o BallBouncer.o -t nes
-ld65 BallBouncer.o -o BallBouncer.nes -t nes
-start BallBouncer.nes
---
+++ build.bat
@@ -0,0 +1,3 @@
+ca65 ./BallBouncer/BallBouncer.asm -o ./BallBouncer/BallBouncer.o -t nes
+ld65 ./BallBouncer/BallBouncer.o -o ./BallBouncer/BallBouncer.nes -t nes
+start ./BallBouncer/BallBouncer.nes
